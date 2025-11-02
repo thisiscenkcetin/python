dc7578725dc6b4e22bb1c7e622dc9bdbfa5ebec9
Cenk Çetin <dev.cenkcetin@gmail.com>
2025-11-02 07:20:58 +0300

    Kendini tanıtma
    Veri Bilimine Giriş- 4. Sınıf

---
+++ Cenk_Cetin.py
@@ -0,0 +1,39 @@
+# Haftalık Ödev - Kendimi Tanıtma
+# Cenk Çetin
+
+# Str 
+ad = "Cenk Çetin"
+dogum_yeri = "İzmir/Buca"
+sehir = "Çanakkale"
+meslek = "Web Developer & Graphic Designer"
+egitim = "Trakya Üniversitesi Web Tasarım ve Kodlama"
+egitim2 = "İstanbul Üniversitesi - Bilgisayar Programcılığı"
+
+# İnt 
+yas = 23
+dogum_yili = 2002
+calisma_yili = 12
+takipci = 40
+ziyaretci = 1200
+
+
+# Tanıtma
+print(f"""
+Merhaba! Ben {ad}, 17 Kasım itibarıyla {yas} yaşındayım.
+{dogum_yeri}'da doğdum, yıllardır {sehir}'de yaşıyorum.
+
+Eğitim sürecim {egitim} ile başladı ve
+{egitim2} son sınıf öğrencisi olarak devam ediyor.
+
+{meslek} olarak {calisma_yili} yıldır çalışıyorum. Genç yaşta başladım ve alaylı olarak mesleki tecrübe kazandım.
+Çanakkale Basın'ın kurucusuyum - Küçük bir şehir olduğumuz için yıllardır büyüme hedefimiz yok, siyasi dönemlerde ek gelir için elimde tutuyorum.
+
+- Racing motor tutkunuyum, motor üstünde özgür olmayı seviyorum.
+- Ayrıca adrenaline bayılıyorum.
+Bazen de kafa dinlemek için zaman zaman deniz ve orman gibi sakin alanlara kaçıyorum. Özellikle az kişinin keşfettiği, bakir deniz kıyılarında kitap okuyorum.
+
+Köpekleri ve kedileri çok severim, hayvanlar beni mıknatıs gibi çeker.
+*Anlaşamadığım tek canlı sivrisinek (:
+
+Hobi olarak 3D yazıcı teknolojileri, n8-json yapay zeka araştırmaları, günlük koşu ve fitness ile ilgileniyorum.
+""")
